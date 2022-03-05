5459fae7ba83015f73d3b9e9a24340c2a366a3ff
Wouter He <2236629703@qq.com>
2022-03-05 16:32:03 +0800

    fix

--- Stack/SqStack.cpp
+++ Stack/SqStack.cpp
@@ -1,34 +1,55 @@
 // g++ SqStack.cpp -o SqStack
 
 #include <iostream>
+#define MaxSize 10
 using namespace std;
 
 struct Stack 
 {
-
+    int data[MaxSize];    // 静态数组存放栈中元素
+    int top;              // 栈顶指针
 };
 
 // 初始化
-void InitStack(Stack &S);
+void InitStack(Stack &S) {
+    S.top = -1;
+};
 
 // 销毁
-void DestroyStack(Stack &S);
+bool DestroyStack(Stack &S) { };
 
 // 进栈
-void Push(Stack &S, int e);
+bool Push(Stack &S, int e) {
+    if (S.top == MaxSize - 1)
+        return false;
+    S.data[++S.top] = e;
+    return true; 
+};
 
 // 出栈
-void Pop(Stack &S, int &e);
+bool Pop(Stack &S, int &e) {
+    if (S.top == -1)
+        return false;
+    e = S.data[S.top--];
+    return true;
+};
 
 // 读取栈顶元素
-void GetTop(Stack S, int &e);
+bool GetTop(Stack S, int &e) {
+    if (S.top == -1)
+        return false;
+    e = S.data[S.top];
+    return true;
+};
 
 // 判空
-bool isEmpty(Stack S);
+bool isEmpty(Stack S) {
+    return (S.top == -1);
+};
 
 int main() {
     Stack S;
-    // InitStack(S);
-    // DestroyStack(S);
+    InitStack(S);
+    DestroyStack(S);
     cin.get();
 }
