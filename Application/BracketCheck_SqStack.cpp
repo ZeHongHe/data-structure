13a86cfa12f3fe09b6a2f7f1f1ef834b9ff6a2d6
Wouter He <2236629703@qq.com>
2022-03-17 21:47:40 +0800

    add

---
+++ Application/BracketCheck_SqStack.cpp
@@ -0,0 +1,17 @@
+// g++ BracketCheck_SqStack.cpp -o BracketCheck_SqStack
+
+#include <iostream>
+#define MaxSize 10
+using namespace std;
+
+typedef struct SqStack {
+    char data[MaxSize];
+    int top;
+} SqStack;
+
+
+
+int main() {
+
+    cin.get();
+}
