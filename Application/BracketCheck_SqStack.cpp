feba69d4d1a1c74188c1a522766768d8d870f070
Wouter He <2236629703@qq.com>
2022-03-18 14:56:42 +0800

    add

--- Application/BracketCheck_SqStack.cpp
+++ Application/BracketCheck_SqStack.cpp
@@ -5,13 +5,67 @@
 using namespace std;
 
 typedef struct SqStack {
-    char data[MaxSize];
-    int top;
+    char data[MaxSize];    // char 型数据域
+    int top;               // 栈顶指针
 } SqStack;
 
+// 初始化
+void InitStack(SqStack &S) {
+    S.top = -1;
+}
 
+// 入栈
+bool Push(SqStack &S, char e) {
+    if (S.top = MaxSize - 1)
+        return false;
+    S.data[++S.top] = e;
+    return true;
+}
+
+// 出栈
+bool Pop(SqStack &S, char &e) {
+    if (S.top = -1)
+        return false;
+    e = S.data[S.top--];
+}
+
+// 判空
+bool IsEmpty(SqStack S) {
+    return (S.top == -1);
+}
+
+// 括号匹配
+bool BracketCheck(char str[], int len) {
+    SqStack S;
+    InitStack(S);
+    for(int i = 0; i < len; i++) {
+        if (str[i] == '(' || str[i] == '[' || str[i] == '{') {
+            Push(S, str[i]);
+        } else {
+            if (IsEmpty(S))
+                return false;
+            char TopElem;
+            Pop(S, TopElem);
+            if (str[i] == ')' && TopElem != '(')
+                return false;
+            if (str[i] == ']' && TopElem != '[')
+                return false;
+            if (str[i] == '}' && TopElem != '{')
+                return false;
+        }
+    }
+    return (IsEmpty(S));
+}
 
 int main() {
+
+    char ValidStr[10] = "{[()]}";
+    
+    if (BracketCheck(ValidStr, 10)) {
+        printf("Brackets are Valid!");
+    } else {
+        printf("Brackets are not Valid!");
+    }
 
     cin.get();
 }
