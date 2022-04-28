ac161886544198b24d5d5678061fa35e123d581a
Wouter He <2236629703@qq.com>
2022-04-28 22:09:28 +0800

    fix: check_color

--- Tree/red_black_tree.cpp
+++ Tree/red_black_tree.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
-#define RB_RED   1
-#define RB_BLACK 0
+#define	RB_RED		0
+#define	RB_BLACK	1
 
 using namespace std;
 
@@ -27,7 +27,7 @@ rb_node *create_node(int val, rb_node *par)
     return node;
 }
 
-rb_node *right_rotate(rb_node *node, rb_node *root)
+rb_node *right_rotate(rb_node *node)
 {
     struct rb_node *left = node->left;
 
@@ -49,17 +49,13 @@ rb_node *right_rotate(rb_node *node, rb_node *root)
             node->par->right = left;
         }
     }
-    else
-    {
-        root = left;
-    }
 
     node->par = left;
 
     return node;
 }
 
-rb_node *left_rotate(rb_node *node, rb_node *root)
+rb_node *left_rotate(rb_node *node)
 {
     struct rb_node *right = node->right;
 
@@ -81,10 +77,6 @@ rb_node *left_rotate(rb_node *node, rb_node *root)
             node->par->right = right;
         }
     }
-    else
-    {
-        root = right;
-    }
 
     node->par = right;
 
@@ -92,41 +84,149 @@ rb_node *left_rotate(rb_node *node, rb_node *root)
     
 }
 
-rb_node *insert_node(int val, rb_node *root)
+void check_color(rb_node *node)
 {
-    rb_node *buffer = root;
-
-    while (buffer)
+    /* 1. if node is root,
+        RB property is intact */
+    if (node->par == NULL)
     {
-        if (val < buffer->val)
+        // node->color = RB_BLACK;
+        return;
+    }
+
+    /* 2. if node or it's parent is black node,
+        RB property is intact */
+    if (node->color == RB_BLACK || node->par->color == RB_BLACK)
+    {
+        return;
+    }
+
+    /* 3. if node and its parent are red nodes,
+        check for uncle */
+    rb_node *parent = node->par;
+    rb_node *grand_parent = parent->par;
+
+    if (grand_parent)
+    {
+        parent->color = RB_BLACK;
+    }
+
+    if (grand_parent->left && grand_parent->left->color == RB_RED &&
+        grand_parent->right && grand_parent->right->color == RB_RED)
+    {
+        grand_parent->color = RB_RED;
+        grand_parent->left->color = grand_parent->right->color = RB_BLACK;
+        return;
+    }
+    else
+    {
+        rb_node *great_grand_parent = grand_parent->par;
+
+        /* right case */
+        if (grand_parent->left = parent)
         {
-            if (buffer->left)
-            {
-                buffer = buffer->left;
+            /* right right case */
+            if (parent->left = node)
+            { 
+                left_rotate(grand_parent);
+
+                parent->color = RB_BLACK;
+                grand_parent->color = RB_RED;
             }
             else
             {
-                rb_node *temp = create_node(val, buffer);
-                buffer->left = temp;
-                buffer = temp;
+                /* right left case */
+                right_rotate(grand_parent);
+
+                left_rotate(great_grand_parent);
+
+                node->color = RB_BLACK;
+                grand_parent = RB_RED;
+            }
+        }
+        else
+        {
+            /* left case */
+            /* left left case */
+            if (parent->right = node)
+            {
+                right_rotate(grand_parent);
+
+                parent->color = RB_BLACK;
+                grand_parent->color = RB_RED;
+            }
+            else
+            {
+                /* left right case */
+                left_rotate(grand_parent);
+
+                right_rotate(great_grand_parent);
+
+                node->color = RB_BLACK;
+                grand_parent = RB_RED;
+            }
+        }
+    }
+
+
+}
+
+void insert_node(int val, rb_node *root)
+{
+    rb_node *buffer_node = root;
+
+    while (buffer_node)
+    {
+        if (val < buffer_node->val)
+        {
+            if (buffer_node->left)
+            {
+                buffer_node = buffer_node->left;
+            }
+            else
+            {
+                rb_node *temp = create_node(val, buffer_node);
+                buffer_node->left = temp;
+                buffer_node = temp;
 
                 break;
             }
         }
         else
         {
-            if (buffer->right)
+            if (buffer_node->right)
             {
-                buffer = buffer->right;
+                buffer_node = buffer_node->right;
             }
             else
             {
-                rb_node *temp = create_node(val, buffer);
-                buffer->right = temp;
-                buffer = temp;
+                rb_node *temp = create_node(val, buffer_node);
+                buffer_node->right = temp;
+                buffer_node = temp;
 
                 break;
             }
+        }
+    }
+
+    while (buffer_node != root)
+    {
+        /* adjusted it's RB property */
+        check_color(buffer_node);
+
+        /* if buffer_node is root after adjusted */
+        if (buffer_node->par == NULL)
+        {
+            root = buffer_node;
+            break;
+        }
+
+        /* if buffer_node isn't root */
+        buffer_node = buffer_node->par;
+
+        if (buffer_node == root)
+        {
+            buffer_node->color = RB_BLACK;
         }
     }
 }
